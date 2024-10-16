46ea74c4ab24241c17bdb93b3a253daa4d7c03ab
James-Lu-none <jamesabcde277@gmail.com>
2024-10-16 17:46:35 +0800

    update 8 HPU hccl test

--- quick_tests/8_HPU/run.sh
+++ quick_tests/8_HPU/run.sh
@@ -4,4 +4,4 @@ THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 cd "$THIS_SCRIPT_PATH"
 
 export PT_HPU_LAZY_MODE=0
-mpirun --allow-run-as-root -np 8 $PYTHON demo.py
+mpirun --allow-run-as-root -np 8 $PYTHON hccl_demo.py
