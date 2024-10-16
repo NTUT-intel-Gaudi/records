e335be7aa6ed31db4bd18f1bf34a8b8d368e3dec
James-Lu-none <jamesabcde277@gmail.com>
2024-10-16 17:06:07 +0800

    add virtual environment setup and activate scripts in run.sh

--- quick_tests/single_HPU/run.sh
+++ quick_tests/single_HPU/run.sh
@@ -3,4 +3,6 @@
 THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
 cd "$THIS_SCRIPT_PATH"
 
+activate
+
 $PYTHON demo.py
---
+++ venv_setup.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+cd ~
+mkdir .venv
+cd .venv
+python3 -m venv myPython
+
+alias activate="source ~/.venv/myPython/bin/activate"
+export $PYTHON=python3
+
+activate
+$PYTHON -m pip install -r requirements.txt
