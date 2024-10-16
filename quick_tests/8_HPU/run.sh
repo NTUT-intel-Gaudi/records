31fff6bcfef8e93c614f656339d126b53346059f
James-Lu-none <jamesabcde277@gmail.com>
2024-10-16 16:41:37 +0800

    update run scripts to change directory to script location before execution

--- quick_tests/8_HPU/run.sh
+++ quick_tests/8_HPU/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
+cd "$THIS_SCRIPT_PATH"
+
 export PT_HPU_LAZY_MODE=0
 mpirun --allow-run-as-root -np 8 $PYTHON demo.py
--- quick_tests/single_HPU/run.sh
+++ quick_tests/single_HPU/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+THIS_SCRIPT_PATH=$(cd "$(dirname "$0")" && pwd)
+cd "$THIS_SCRIPT_PATH"
+
 $PYTHON demo.py
