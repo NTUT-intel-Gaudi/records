d8e8daefd3eff60743eb555ae7faa4afbe35064a
James-Lu-none <jamesabcde277@gmail.com>
2024-11-03 10:38:26 +0800

    feat: add SPSC.sh for single prompt single card testing

---
+++ optimum-habana/text-generation/tests/SPSC.sh
@@ -0,0 +1,9 @@
+# Single prompt Single card test
+python run_generation.py \
+--model_name_or_path /root/workspace/model/meta-llama/Llama-3.1-8B/ \
+--use_hpu_graphs \
+--use_kv_cache \
+--max_new_tokens 100 \
+--do_sample \
+--bf16 \
+--prompt "hello world"
