05640a554df6382c68a00bf441abc1d6dd02ffdd
James-Lu-none <jamesabcde277@gmail.com>
2024-11-03 21:03:49 +0800

    feat: update SPSC.sh to change directory for text generation tests

--- optimum-habana/text-generation/tests/SPSC.sh
+++ optimum-habana/text-generation/tests/SPSC.sh
@@ -1,4 +1,5 @@
 # Single prompt Single card test
+cd /root/workspace/github/optimum-habana/examples/text-generation/
 python run_generation.py \
 --model_name_or_path /root/workspace/model/meta-llama/Llama-3.1-8B/ \
 --use_hpu_graphs \
